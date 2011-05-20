7406b3b05f3336999c30aa8d4cc36cbd6c32b782
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-05-20 23:49:13 +0200

    Added check for build.js in current directory

--- src/main/bootstrap.js
+++ src/main/bootstrap.js
@@ -1,6 +1,10 @@
 (function (args) {
 	var main = new Make.Main();
 	main.initGlobalScope(this);
-	Make.Sys.loadJavascriptFile('build.js');
-	main.run(args);
+	if (!Make.Sys.fileExists('build.js')) {
+		Make.Sys.log('File build.js not found in current directory.');
+	} else {
+		Make.Sys.loadJavascriptFile('build.js');
+		main.run(args);
+	}
 }(arguments));
