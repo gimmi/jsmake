bbdb5778732f3984875b6821282e1450199f7d80
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-05-30 10:46:30 +0200

    JSDoc on jsmake.CommandRunner

--- src/main/jsmake.CommandRunner.js
+++ src/main/jsmake.CommandRunner.js
@@ -1,13 +1,26 @@
+/**
+ * Don't instantiate it directly, use {@link jsmake.Sys.createRunner}
+ * @constructor
+ */
 jsmake.CommandRunner = function (command) {
 	this._command = command;
 	this._arguments = [];
 	this._logger = jsmake.Sys;
 };
 jsmake.CommandRunner.prototype = {
+	/**
+	 * Add all passed arguments. Supports any number of parameters.
+	 * @returns {jsmake.CommandRunner} this instance, for chaining calls
+	 * @example
+	 * jsmake.Sys.createRunner('cmd.exe').args('par1', 'par2', [ 'par3', 'par4' ]).run();
+	 */
 	args: function () {
 		this._arguments = this._arguments.concat(jsmake.Utils.flatten(arguments));
 		return this;
 	},
+	/**
+	 * Run configured command. if exitstatus of the command is 0 then execution is considered succesful, otherwise an exception is thrown
+	 */
 	run: function () {
 		this._logger.log(this._command + ' ' + this._arguments.join(' '));
 		var exitStatus = jsmake.Sys.runCommand(this._command, { args: this._arguments });
