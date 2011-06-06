b83254c1c293747e3be639cb83779021605cbf11
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-06-06 16:23:42 +0200

    Refactoring

--- src/main/jsmake.js
+++ src/main/jsmake.js
@@ -5,16 +5,10 @@ var sys = require('jsmake/sys');
 var _project = new project.Project('a project', 'default', function () {}, sys.Sys);
 
 exports.task = function (name, tasks, body) {
-	if (!_project) {
-		throw 'no project defined';
-	}
 	_project.addTask(new task.Task(name, tasks, body, sys.Sys));
 };
-exports.getProject = function () {
-	if (!_project) {
-		throw 'no project defined';
-	}
-	return _project;
+exports.runTask = function (name, args) {
+	return _project.runTask(name, args);
 };
 exports.Utils = require('jsmake/utils').Utils;
 exports.Fs = require('jsmake/fs').Fs;
