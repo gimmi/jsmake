c8b3a09557a0ba48d8c110fa2cb9ce45a714b5eb
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-09-27 21:02:14 +0200

    Added commonjs commandline options

--- build.cmd
+++ build.cmd
@@ -1,1 +1,5 @@
-java -cp lib\main\rhino-1.7r3\js.jar org.mozilla.javascript.tools.shell.Main build.js %*
+@echo off
+rem Workaround for https://github.com/mozilla/rhino/issues/10
+set jsmakemodules=%~dp0
+set jsmakemodules=%jsmakemodules:\=/%
+java -cp lib\main\rhino-1.7r3\js.jar org.mozilla.javascript.tools.shell.Main -modules "file:///%jsmakemodules%" -modules "." build.js %*
--- src/main/jsmake.cmd
+++ src/main/jsmake.cmd
@@ -1,1 +1,5 @@
-java -cp "%~dp0js.jar" org.mozilla.javascript.tools.shell.Main "%~dp0bootstrap.js" "%~dp0jsmake.js" %*
+@echo off
+rem Workaround for https://github.com/mozilla/rhino/issues/10
+set jsmakemodules=%~dp0
+set jsmakemodules=%jsmakemodules:\=/%
+java -cp "%~dp0js.jar" org.mozilla.javascript.tools.shell.Main -modules "file:///%jsmakemodules%" -modules "." "%~dp0bootstrap.js" "%~dp0jsmake.js" %*
