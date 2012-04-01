00179817b0a4946bc5c73129317d933f6a07b66f
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2012-04-01 19:20:14 +0200

    added internal method jsmake.Sys._buildRunConfig to parse parameters of the new run command

--- src/main/jsmake.Sys.js
+++ src/main/jsmake.Sys.js
@@ -50,5 +50,25 @@ jsmake.Sys = {
 			return def;
 		}
 		throw 'Environment variable "' + name + '" not defined.';
+	},
+	_buildRunConfig: function () {
+		var args = jsmake.Utils.flatten(arguments);
+
+		if (jsmake.Utils.isString(args[0])) {
+			args = {
+				cmd: args.shift(),
+				args: args
+			};
+		} else {
+			args = args[0];
+		}
+		
+		return {
+			cmd: jsmake.Utils.isString(args.cmd) ? args.cmd : '',
+			args: jsmake.Utils.flatten(args.args),
+			successCodes: jsmake.Utils.isEmpty(args.successCodes) ? [0] : jsmake.Utils.flatten(args.successCodes),
+			failOnError: jsmake.Utils.isEmpty(args.failOnError) ? true : !!args.failOnError,
+			captureOutput: jsmake.Utils.isEmpty(args.captureOutput) ? false : !!args.captureOutput
+		};
 	}
 };
--- src/test/jsmake.SysTest.js
+++ src/test/jsmake.SysTest.js
@@ -17,4 +17,54 @@ describe("jsmake.Sys", function () {
 			target._getEnvVar('name', null, undefined);
 		}).toThrow('Environment variable "name" not defined.');
 	});
+	
+	describe('run', function () {
+		it('should build configuration from simple parameters', function () {
+			expect(target._buildRunConfig('COMMAND')).toEqual({
+				cmd: 'COMMAND',
+				args: [],
+				successCodes: [0],
+				failOnError: true,
+				captureOutput: false
+			});
+
+			expect(target._buildRunConfig('COMMAND', 'p1', ['p2', 'p3'])).toEqual({
+				cmd: 'COMMAND',
+				args: ['p1', 'p2', 'p3'],
+				successCodes: [0],
+				failOnError: true,
+				captureOutput: false
+			});
+		});
+		
+		it('should build cmd parameter', function () {
+			expect(target._buildRunConfig({}).cmd).toEqual('');
+			expect(target._buildRunConfig({ cmd: 'COMMAND' }).cmd).toEqual('COMMAND');
+			expect(target._buildRunConfig({ cmd: 123 }).cmd).toEqual('');
+		});
+		
+		it('should build args parameter', function () {
+			expect(target._buildRunConfig({}).args).toEqual([]);
+			expect(target._buildRunConfig({ args: 'p1' }).args).toEqual(['p1']);
+			expect(target._buildRunConfig({ args: ['p1', ['p2', 'p3']] }).args).toEqual(['p1', 'p2', 'p3']);
+		});
+		
+		it('should build successCodes parameter', function () {
+			expect(target._buildRunConfig({}).successCodes).toEqual([0]);
+			expect(target._buildRunConfig({ successCodes: 3 }).successCodes).toEqual([3]);
+			expect(target._buildRunConfig({ successCodes: [1, [2, 3]] }).successCodes).toEqual([1, 2, 3]);
+		});
+		
+		it('should build failOnError parameters', function () {
+			expect(target._buildRunConfig({}).failOnError).toEqual(true);
+			expect(target._buildRunConfig({ failOnError: false }).failOnError).toEqual(false);
+			expect(target._buildRunConfig({ failOnError: 'T' }).failOnError).toEqual(true);
+		});
+		
+		it('should build captureOutput parameters', function () {
+			expect(target._buildRunConfig({}).captureOutput).toEqual(false);
+			expect(target._buildRunConfig({ captureOutput: true }).captureOutput).toEqual(true);
+			expect(target._buildRunConfig({ captureOutput: 0 }).captureOutput).toEqual(false);
+		});
+	});
 });
